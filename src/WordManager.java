aedbab10646a0e885618c15bc2ab3c33eb01eef4
hmitwaly <hmitwaly@uwaterloo.ca>
2016-05-14 12:26:00 -0400

    Update WordManager.java

--- src/WordManager.java
+++ src/WordManager.java
@@ -1,18 +1,18 @@
 
 public class WordManager {
-	
+//List of words given in the game
 private String[] words = {"Ant","Bird","Cat","Chicken","Cow","Dog","Elephant","Kangaroo","Rabbit","Whale","Zebra"};
-	
+	//gives the list of words to whoever asks for it
 	public String[] getWords() {
 		return this.words;
 	}
-	
+	//randomly selects a word for the game
 	public String SelectWord() {
 		int position =(int) (Math.random()*words.length);
 		String word = words[position];
 		return word;
 	}
-	
+	//changes the word into letters
 	public char[] convertWord(String word) {
 		int numOfLetters = word.length();
 		char[] charWord = new char[numOfLetters];
@@ -21,7 +21,7 @@ private String[] words = {"Ant","Bird","Cat","Chicken","Cow","Dog","Elephant","K
 		}
 		return charWord;
 	}
-	
+	//changes the letters into dashes for the game
 	private char[] convertToDash (char[] word) {
 		int numOfDashes = word.length;
 		char[] dashes = new char[numOfDashes];
@@ -31,7 +31,7 @@ private String[] words = {"Ant","Bird","Cat","Chicken","Cow","Dog","Elephant","K
 		return dashes;
 		
 	}
-	
+	//gives the final product to the game
 	public char[] giveWord (char[] word) {
 		return convertToDash(word);
 	}
